e0305752649adcfec099bc5dd2c7fae4ba6e5638
Kaddour <matthias-mehdi.kaddour@epita.fr>
2022-09-30 13:33:39 +0200

    f com

---
+++ matrix/matrix.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+void add(double m1[], double m2[], size_t rows, size_t cols, double r[])
+{
+    
+}
+
+void mul(double m1[], double m2[], size_t r1, size_t c1, size_t c2, double r[])
+{
+    // TODO
+}
